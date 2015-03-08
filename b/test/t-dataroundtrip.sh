8f88f9951fafc5ae7044ad743a82829a7294dc25
Jonas Schneider <mail@jonasschneider.com>
2015-03-08 20:37:42 +0100

    more reasonable makefile

--- b/test/t-dataroundtrip.sh
+++ b/test/t-dataroundtrip.sh
@@ -6,12 +6,15 @@ shopt -s dotglob
 
 tmp=$(mktemp -d /tmp/haven-testXXXXX)
 repo=$tmp/repo
+data=$tmp/data
+mkdir $data
+dd if=/dev/urandom of=$data/x bs=512 count=8
 
 gdrive_folder_name="haven-test $(date)"
 haven-b-init $repo $gdrive_folder_name > $tmp/backupspec.json
 
-haven-b-backup $tmp/backupspec.json test/data firstbackup
-haven-b-backup $tmp/backupspec.json test/data secondbackup
+haven-b-backup $tmp/backupspec.json $data firstbackup
+haven-b-backup $tmp/backupspec.json $data secondbackup
 
 # now, a crash happens
 rm -fr $tmp/repo
@@ -19,7 +22,7 @@ rm -fr $tmp/repo
 # now attempt to restore
 haven-b-restore $tmp/backupspec.json secondbackup $tmp/restored
 
-expected=$(cd test/data; tar c * | sha1sum)
+expected=$(cd $data; tar c * | sha1sum)
 actual=$(cd $tmp/restored; tar c * | sha1sum)
 
 if [ "$expected" != "$actual" ]; then

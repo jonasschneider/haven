f3811d0f2b1dce90ddf720593803f8bb60fb9bf6
Jonas Schneider <mail@jonasschneider.com>
2015-04-10 01:44:27 +0200

    add dummy gpg values

--- b/test/t-dataroundtrip.sh
+++ b/test/t-dataroundtrip.sh
@@ -17,18 +17,19 @@ zfs snapshot $pool/data@2
 expected=$(cd /$pool/data; tar c * | sha1sum)
 haven-b-backup $pool/data@2 secondbackup > /tmp/bupname
 
-sleep 4
+sleep 2
 # now, a crash happens
 zfs destroy -fr $pool/data
 
 # now attempt to restore
 haven-b-gdrive download --stdout -i $(cat /tmp/bupname) | unxz | zfs recv $pool/data@2
 
-actual=$(cd /$pool/data/.zfs/snapshots/2; tar c * | sha1sum)
+actual=$(cd /$pool/data/.zfs/snapshot/2; tar c * | sha1sum)
 
 if [ "$expected" != "$actual" ]; then
   echo expected $expected, but got $actual
   exit 1
 fi
 
-rm -fr $tmp
+sleep 2
+zfs destroy -fr $pool/data

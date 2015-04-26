cd24ddbf83dbe31a9cac5d035f04b96b48096c2e
Jonas Schneider <mail@jonasschneider.com>
2015-04-27 00:34:02 +0200

    b: instruct how to build via native platform

--- b/test/platform/native.sh
+++ b/test/platform/native.sh
@@ -1,1 +1,7 @@
+# install the PPA here explicitly so we don't always install it in `test/harness`
+sudo apt-add-repository --yes ppa:zfs-native/stable
+sudo apt-get update
+sudo apt-get -y install buffer gnupg-agent spl-dkms
+sudo apt-get -y install ubuntu-zfs
+
 export pool=$(basename $vdev)
